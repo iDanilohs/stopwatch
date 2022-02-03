a551258fbba16e74f9670eb4f49211bda35daf94
Danilo Herrera <daniloherrera1702@gmail.com>
2022-02-03 10:15:53 -0500

    I added the logic of the stopwatch

--- stopwatch.py
+++ stopwatch.py
@@ -1,20 +1,37 @@
+from time import sleep
+
+
+def show(hours, minutes, seconds):
+    print(f'{hours}h : {minutes}m : {seconds}s')
+
+
 def run():
     status = int(input(
         "Hola ¿Cómo estas? Presiona 1: para iniciar el cronometro. Presiona 2: Para detenerlo."))
 
-    while status == 1:
+    seconds = 0
+    minutes = 0 
+    hours = 0
 
-        seconds = 0
-        minutes = 0 
-        hours = 0
+    while status == 1 or hours <= 60:
+        seconds += 1
+        show(hours, minutes, seconds)
+        sleep(1)
 
-        for seconds in range(62):
-            print(f'{hours} : {minutes} : {seconds}')
-            if seconds > 60:
-                minutes += 1
-                if minutes > 60
-
-
+        if seconds == 60:
+            minutes += 1
+            seconds = 0
+            show(hours, minutes, seconds)
+            sleep(1)
+        elif minutes == 60:
+            hours += 1
+            minutes = 0
+            show(hours, minutes, seconds)
+            sleep(1)
+        elif hours == 60:
+            print("60 horas es el maximo de tiempo")
+            break
+        
 
 if __name__ == '__main__':
     run()
